155d895150ca9674f86398ab0d0e03477712df86
oussama chaouki pc <oussama11chaouki@gmail.com>
2024-07-26 22:18:58 +0100

    second

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author OUSSAMA CHAOUKI <https://github.com/oussama7chaouki>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
